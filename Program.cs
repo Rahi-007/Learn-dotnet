640c21463c17f3ea1ccb3320d4cd9f8f03edcacd
Rahi-007 <rubaiedahammed007@gmail.com>
2026-05-27 01:10:44 +0600

    learn basic syntax

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("My first console app!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Rubaied Ahammed Rahi. ");
+        Console.WriteLine("I Am 20 Years old");
+        Console.WriteLine("I am a full stack developer");
+    }
+}
