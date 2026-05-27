fe152c9647f23dd9bd08c85139996a908fd1d40a
Rahi-007 <rubaiedahammed007@gmail.com>
2026-05-27 14:08:48 +0600

    learn arithmetic operator

--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-
-class Program
-{
+using System.Globalization;
+class Program {
     public static void Main(string[] args)
     {
-        Console.Write("Rubaied Ahammed Rahi. ");
-        Console.WriteLine("I Am 20 Years old");
-        Console.WriteLine("I am a full stack developer");
+    double num1;
+    double num3;
+    double num2;
+    double result;
+
+    Console.Write("Enter 1st number ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter 2nd number ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter 3rd number ");
+    num3 = Convert.ToDouble(Console.ReadLine());
+
+    result = num1 + num2 + num3;
+    Console.WriteLine($"Addition of three number {num1} + {num2} + {num3} = {result:F6}");
+
+    result = num1 - num2 - num3;
+    Console.WriteLine($"Subtraction of three number {num1} - {num2} - {num3} = {result:F6}");
+
+    result = num1 * num2 * num3;
+    Console.WriteLine($"Multiplication of three number {num1} * {num2} * {num3} = {result:F6}");
+
+    result = num1 / num2 / num3;
+    Console.WriteLine($"Division of three number {num1} / {num2} / {num3} = {result:F6}");
+
+    result = num1 % num2 % num3;
+    Console.WriteLine($"Remainder of three number {num1} % {num2} % {num3} = {result:F6}");
+
+    result = (num1 + num2 + num3) / 3;
+    Console.WriteLine($"Average of three number {num1} % {num2} % {num3} = {result:F6}");
     }
 }
