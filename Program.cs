e331ef580d0f72399d06a8aaa65ae4fc34fb564e
Rahi-007 <rubaiedahammed007@gmail.com>
2026-05-30 14:31:21 +0600

    learn unary operator

--- Program.cs
+++ Program.cs
@@ -3,43 +3,25 @@ class Program
 {
     public static void Main(string[] args)
     {
-        double mark;
+        int num, num2;
 
-        Console.Write("Enter Your Mark: ");
-        mark = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter 1st number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter 2nd number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
 
-        if (mark > 0 && mark < 100)
-        {
-            char grade;
-            if (mark >= 90)
-            {
-                grade = 'A';
-            }
-            else if (mark >= 80)
-            {
-                grade = 'B';
-            }
-            else if (mark >= 70)
-            {
-                grade = 'C';
-            }
-            else if (mark >= 60)
-            {
-                grade = 'D';
-            }
-            else if (mark >= 50)
-            {
-                grade = 'E';
-            }
-            else
-            {
-                grade = 'F';
-            }
-            Console.WriteLine($"You got {grade}");
-        }
-        else
-        {
-            Console.WriteLine($"{mark} is Invalid");
-        }
+        string binary = Convert.ToString(num, 2);
+        string binary2 = Convert.ToString(num2, 2);
+        Console.WriteLine($"Binary numbers: {binary}, {binary2}");
+
+        int numAnd = num & num2;
+        int numOr = num | num2;
+        int numShift = num << num2;
+        int numUnShift = num >> num2;
+        Console.WriteLine($"bit And Operator number: {binary} & {binary2} = {Convert.ToString(numAnd, 2)}");
+        Console.WriteLine($"bit Or Operator number: {binary} | {binary2} = {Convert.ToString(numOr, 2)}");
+        Console.WriteLine($"bit Shift Operator number: {binary} << {binary2} = {Convert.ToString(numShift, 2)}");
+        Console.WriteLine($"bit UnShift Operator number: {binary} >> {binary2} = {Convert.ToString(numUnShift, 2)}");
+
     }
 }
