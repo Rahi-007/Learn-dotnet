c83c640f72b1dd7e26d6470a33e5771307619993
Rahi-007 <rubaiedahammed007@gmail.com>
2026-05-31 13:13:33 +0600

    learn Constructor and keywords

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
 ﻿using System;
+
+class Person
+{
+    private readonly string name = "";
+    private readonly int age = 0;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        Console.Write($"Name: {this.name}, \n age: {this.age} \n");
+    }
+
+    public int Age
+    {
+        set;
+        get;
+    }
+
+    public string Name
+    {
+        set;
+        get;
+    }
+}
 class Program
 {
     public static void Main(string[] args)
     {
-        int num, num2;
-
-        Console.Write("Enter 1st number: ");
-        num = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter 2nd number: ");
-        num2 = Convert.ToInt32(Console.ReadLine());
-
-        string binary = Convert.ToString(num, 2);
-        string binary2 = Convert.ToString(num2, 2);
-        Console.WriteLine($"Binary numbers: {binary}, {binary2}");
-
-        int numAnd = num & num2;
-        int numOr = num | num2;
-        int numShift = num << num2;
-        int numUnShift = num >> num2;
-        Console.WriteLine($"bit And Operator number: {binary} & {binary2} = {Convert.ToString(numAnd, 2)}");
-        Console.WriteLine($"bit Or Operator number: {binary} | {binary2} = {Convert.ToString(numOr, 2)}");
-        Console.WriteLine($"bit Shift Operator number: {binary} << {binary2} = {Convert.ToString(numShift, 2)}");
-        Console.WriteLine($"bit UnShift Operator number: {binary} >> {binary2} = {Convert.ToString(numUnShift, 2)}");
-
+        new Person("Rahi", 20);
     }
 }
