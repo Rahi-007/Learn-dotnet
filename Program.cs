5ae6877d2cc51bd569e9c485936c32b78aa0bb46
Rahi-007 <rubaiedahammed007@gmail.com>
2026-06-01 13:34:40 +0600

    learn array

--- Program.cs
+++ Program.cs
@@ -1,33 +1,46 @@
 ﻿using System;
 
-class Person
-{
-    private readonly string name = "";
-    private readonly int age = 0;
-
-    public Person(string name, int age)
-    {
-        this.name = name;
-        this.age = age;
-        Console.Write($"Name: {this.name}, \n age: {this.age} \n");
-    }
-
-    public int Age
-    {
-        set;
-        get;
-    }
-
-    public string Name
-    {
-        set;
-        get;
-    }
-}
 class Program
 {
     public static void Main(string[] args)
     {
-        new Person("Rahi", 20);
+        Console.Write($"Enter a Array size: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] numbers = new int[size];
+
+        // array input
+        for (int index = 0; index < size; index++)
+        {
+            Console.Write($"{index + 1}. Enter: ");
+            numbers[index] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write($"The array => ");
+
+        // print array
+        foreach (int num in numbers)
+        {
+            Console.Write($"{num}, ");
+        }
+
+        // find min and max
+        int min = numbers[0];
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (min > num)
+            {
+                min = num;
+            }
+            if (max < num)
+            {
+                max = num;
+            }
+        }
+
+        // print max and min
+        Console.WriteLine($"");
+        Console.WriteLine($"Min: {min}");
+        Console.WriteLine($"Max: {max}");
     }
 }
