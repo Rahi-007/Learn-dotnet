d9eb892e0f33af3efa15e942670dd5cd911c0bac
Rahi-007 <rubaiedahammed007@gmail.com>
2026-06-06 23:38:25 +0600

    Add Two Numbers

--- Program.cs
+++ Program.cs
@@ -1,46 +1,19 @@
 ﻿using System;
+using System.Collections.Immutable;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        Console.Write($"Enter a Array size: ");
-        int size = Convert.ToInt32(Console.ReadLine());
-        int[] numbers = new int[size];
-
-        // array input
-        for (int index = 0; index < size; index++)
-        {
-            Console.Write($"{index + 1}. Enter: ");
-            numbers[index] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        Console.Write($"The array => ");
-
-        // print array
-        foreach (int num in numbers)
-        {
-            Console.Write($"{num}, ");
-        }
-
-        // find min and max
-        int min = numbers[0];
-        int max = numbers[0];
-        foreach (int num in numbers)
-        {
-            if (min > num)
-            {
-                min = num;
-            }
-            if (max < num)
-            {
-                max = num;
-            }
-        }
-
-        // print max and min
-        Console.WriteLine($"");
-        Console.WriteLine($"Min: {min}");
-        Console.WriteLine($"Max: {max}");
+        // Add Two Numbers =>
+        Console.WriteLine($"Add Two Numbers => {string.Join(", ", AddTwoNumbers([2,3,4], [3,5,3]))}");
+        Console.ReadKey();
+    }
+    public static int[] AddTwoNumbers(int[] l1, int[] l2) {
+        Array.Reverse(l1);
+        Array.Reverse(l2);
+        Console.WriteLine(string.Join(", ", l1));
+        Console.WriteLine(string.Join(", ", l2));
+        return [];
     }
 }
