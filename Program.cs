62d0272fe824702460f31b8458f070153d8219b7
Rahi-007 <rubaiedahammed007@gmail.com>
2026-06-28 23:34:50 +0600

    updated

--- Program.cs
+++ Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Immutable;
 
 class Program
 {
+
+
     public static void Main(string[] args)
     {
-        // Add Two Numbers =>
-        Console.WriteLine($"Add Two Numbers => {string.Join(", ", AddTwoNumbers([2,3,4], [3,5,3]))}");
+        Console.WriteLine($"Add Two Numbers => {string.Join(", ", AddTwoNumbers([2, 3, 4, 4], [3, 5, 3]))}");
         Console.ReadKey();
     }
-    public static int[] AddTwoNumbers(int[] l1, int[] l2) {
-        Array.Reverse(l1);
-        Array.Reverse(l2);
-        Console.WriteLine(string.Join(", ", l1));
-        Console.WriteLine(string.Join(", ", l2));
-        return [];
-    }
+
 }
